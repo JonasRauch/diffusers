60b3d1795dc4c34128af6a02af007c51873f48ab
Jonas Rauch <jonas.rauch@googlemail.com>
2022-10-06 12:00:05 +0200

    fix data_dir argument

--- examples/dreambooth_multiple/train_dreambooth_multiple.py
+++ examples/dreambooth_multiple/train_dreambooth_multiple.py
@@ -174,7 +174,7 @@ def parse_args():
     if env_local_rank != -1 and env_local_rank != args.local_rank:
         args.local_rank = env_local_rank
 
-    if args.instance_data_dir is None:
+    if args.data_dir is None:
         raise ValueError("You must specify a train data directory.")
 
     return args
